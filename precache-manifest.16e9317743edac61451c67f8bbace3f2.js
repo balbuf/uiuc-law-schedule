f142ed71df9f28e832f3cd37540dd1f365c3ca0e
balbuf <balbuf@users.noreply.github.com>
2021-11-02 23:26:22 +0000

    Deploying to gh-pages from @ balbuf/uiuc-law-schedule@b7561ea2d44dffafcf44532875cb0a344dd458bf 🚀

---
+++ precache-manifest.16e9317743edac61451c67f8bbace3f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57356cc7dfeba19a7f14987d97d4e663",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abc37ace68b54f9e51a8",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b39db804ff420a7797b1",
+    "url": "./static/js/2.e11aa2b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.e11aa2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc37ace68b54f9e51a8",
+    "url": "./static/js/main.3adb3960.chunk.js"
+  },
+  {
+    "revision": "fee08a1b545e397da9c9",
+    "url": "./static/js/runtime-main.343313b8.js"
+  }
+]);
